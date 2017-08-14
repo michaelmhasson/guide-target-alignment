09e114b044c87384a6d7c0d4e8ce3c06ef05dd62
michaelmhasson <30603332+michaelmhasson@users.noreply.github.com>
2017-08-14 16:44:33 -0400

    Update affalign.py

--- affalign.py
+++ affalign.py
@@ -1,47 +1,52 @@
 start = -5
 extend = -1
 
-#determine if there is a match or mismatch at given position
-def sim(s1,s2,i,j):
+
+# determine if there is a match or mismatch at given position
+def sim(s1, s2, i, j):
     if s1[i-1] == s2[j-1]:
         return 1
     else:
         return -1
 
-#initialization fxns for the three matrices
-def inx(i,j):
+
+# initialization fxns for the three matrices
+def inx(i, j):
     if j > 0 and i == 0:
-        return [-float('inf'),0]
+        return [-float('inf'), 0]
     elif j == 0:
-        return [start + extend * i,2]
+        return [start + extend * i, 2]
     else:
-        return [0,0]
+        return [0, 0]
 
-def iny(i,j):
+
+def iny(i, j):
     if i > 0 and j == 0:
-        return [-float('inf'),0]
+        return [-float('inf'), 0]
     elif i == 0:
-        return [start + extend * j,3]
+        return [start + extend * j, 3]
     else:
-        return [0,0]
+        return [0, 0]
 
-def inm(i,j):
+
+def inm(i, j):
     if i == 0 and j == 0:
-        return [0,0]
+        return [0, 0]
     elif i == 0 or j == 0:
-        return [-float('inf'),0]
+        return [-float('inf'), 0]
     else:
-        return [0,0]
+        return [0, 0]
 
-#using the fxns to make alignment matrix
-def alignmat(s1,s2):
+
+# using the fxns to make alignment matrix
+def alignmat(s1, s2):
     l1 = len(s1) + 1
     l2 = len(s2) + 1
     X = [[inx(i, j) for j in range(0, l2)] for i in range(0, l1)]
     Y = [[iny(i, j) for j in range(0, l2)] for i in range(0, l1)]
     M = [[inm(i, j) for j in range(0, l2)] for i in range(0, l1)]
-    for j in range(1,l2):
-        for i in range(1,l1):
+    for j in range(1, l2):
+        for i in range(1, l1):
             if (start + M[i-1][j][0]) > X[i-1][j][0]:
                 X[i][j] = [extend + start + M[i-1][j][0],1]
             else:
@@ -60,6 +65,7 @@ def alignmat(s1,s2):
     s1a = ''
     s2a = ''
     adata = ''
+    insstr = ''
     ins = 0
     istart = -10
     delete = 0
@@ -71,12 +77,11 @@ def alignmat(s1,s2):
     inp = [M[i][j][1], X[i][j][1], Y[i][j][1]]
     p = inp[inv.index(max(inv))]
     score = max(inv)
-    def newptr(p,i,j):
+
+    def newptr(p, i, j):
         inp = [M[i][j][1], X[i][j][1], Y[i][j][1]]
         return inp[p-1]
     while i > 0 or j > 0:
-
-        #print(str(p) + ' ' + str(i) + ' ' + str(j))
         if p == 1:
             s1a = s1[i-1] + s1a
             s2a = s2[j-1] + s2a
@@ -100,17 +105,11 @@ def alignmat(s1,s2):
         else:
             s1a = '-' + s1a
             s2a = s2[j-1] + s2a
+            insstr = s2[j-1] + insstr
             adata = 'i' + adata
             ins += 1
             if istart == -10:
                 istart = j-1
             p = newptr(p, i, j)
             j -= 1
-    return [s1a,s2a,adata,str(ins),str(delete),str(mismatch),str(score),str(dstart),str(istart)]
-'''
-[s1a,s2a,adata,i,d,mm,score] = alignmat("GCTTTTTTTCTCGAGTACTAGATATCGGAGTTGTTGTTCGAGGATCCATTAGGCGGCCGC","GCTTTTTTTCTCGAGTACGAGATATCGGAGTTGTTCCATTAGGCGGCCGC")
-print(s1a)
-print(s2a)
-print(adata)
-print('inserts: ' + i + ' deletes: ' + d + ' mismatches: ' + mm + ' score: ' + score)
-'''
+    return [s1a, s2a, adata, str(ins), str(delete), str(mismatch), str(score), str(dstart), str(istart), insstr]
